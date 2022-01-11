e89d22221cefc4a7523dff425becf1282cfb0036
panu206 <pranavbhendawade@gmail.com>
2022-01-11 06:19:14 +0530

    third program

---
+++ ex3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[]){
+
+	int weight = 80;
+	int height = 173;
+
+	printf("My height is %d centemeters\n", height);
+	printf("My weight is %d kilograms\n", weight);
+
+	return 0;
+}
